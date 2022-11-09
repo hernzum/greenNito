a5ce4dc253447a38a4d0a7bd11fedd1d8a07e91a
Hernán M <325008+hernzum@users.noreply.github.com>
2022-11-09 05:50:18 +0100

    Update greenNito.ino

--- greenNito.ino
+++ greenNito.ino
@@ -1,29 +1,26 @@
   
+    
   
-  /* LM35 analog temperature sensor - conecta una resitencia de 220 Ohm entre GND y la salida del sensor 
-   Led Amarillo  pin---> D6 
+  /* LM35 analog temperature sensor with Arduino example code. More info: https://www.makerguides.com */
+  /* Led Amarillo  pin---> D6 
     Led Verde  pin---> D7
     Led Rojo  pin---> D8
     Servo  pin---> D9
     Water Pump  pin---> D2
     ventilador  pin---> D3
     Relay for Luces ---> D4
-    Temp LM35DT  pin---> A0
+    Temp LM35Dt  pin---> A0
     LDR   pin--->A1
     Soil sensor  pin---> A2 --> commercial
     DIYSoil sensor pin---> A2 ---> DIY
-    Led Verde flash = humedad de la tierra ok , no necesita agua
-    Led Verde On = Bomba de agua activada 
-    Led Amarillo flash = poca luz 
-    Led Rojo = Temperatura sobre 27 grados , activa ventilador y servo para abrir ventana
   */
-  //  ------------ INICIA codigo--------//
-  
   #include <LM35.h> // LM35DT -- version Latest
   #include <Servo.h>   // Servo - Version: Latest 
+  const bool debug = 1; //set to 1 to enable serial debugging
+
   int rojo_temp_pin = 8; //led rojo para mostar temp sobre 27 grados
-  int verde_mojado_pin = 7; // led verde para para q esta regado
-  int amarillo_luz_pin = 6; // led amarillo mostrar q necesita agua
+  int verde_mojado_pin = 7; // led verde para para mostar q esta regado
+  int amarillo_seco_pin = 6; // led amarillo mostrar q necesita agua
   int pump = 2;
   int fan = 3;
   int luz = 4;
@@ -34,18 +31,18 @@
   int Ldr; // variable para sensor de luz
   int SoilSensor;// Variable para sensor humedad en tierra.
   int TempC; // Variable para sensor de Temperatura LM35
-  
   unsigned long  moistureTest; // Variable para refrescar datos del sensor de suelo
   unsigned long ldrTest; // variable para refrescar sensor de Luz
   unsigned long sampleTemp; //variable para refrscar sensor de  Temperatura
   Servo myservo;  // Variable usada para el Servo
   LM35 tempsensor(0);
-  int ledState = LOW;
+  
   void setup() {
-    Serial.begin(9600);  // Begin serial communication at a baud rate of 9600:
+   // Serial.begin(9600);  // Begin serial communication at a baud rate of 9600:
+    if(debug) Serial.begin(9600); //turn on serial for debugging or monitoring values
     pinMode(rojo_temp_pin, OUTPUT);
     pinMode(verde_mojado_pin, OUTPUT);
-    pinMode(amarillo_luz_pin, OUTPUT);
+    pinMode(amarillo_seco_pin, OUTPUT);
     pinMode(pump, OUTPUT);
     pinMode(fan, OUTPUT);
     pinMode(luz, OUTPUT);
@@ -74,7 +71,7 @@
   
     //------------------ Testea la Cantidad de Luz--------------//
   
-    if (millis() - ldrTest > 5000UL)  // Sondeamos la luz cada 5 segundos
+    if (millis() - ldrTest > 3000UL)  // Sondeamos la luz cada 5 segundos
     {
       detector_luz();
       ldrTest = millis();
@@ -92,13 +89,13 @@
     
     //---------- Muestra los datos en la com serial -------//
   
-    Serial.print(Ldr);
-    Serial.println(" lux");
-    Serial.print(SoilSensor);
-    Serial.println(" % humedad");
-    Serial.print(TempC);                            // Print the temperature in the Serial Monitor:
-    Serial.print(" \xC2\xB0");                        // shows degree symbol
-    Serial.println("C");
+    if(debug)Serial.print(Ldr);
+    if(debug)Serial.println(" lux");
+    if(debug)Serial.print(SoilSensor);
+    if(debug)Serial.println(" % humedad del suelo");
+    if(debug)Serial.print(TempC);                            // Print the temperature in the Serial Monitor:
+    if(debug)Serial.print(" \xC2\xB0");                        // shows degree symbol
+    if(debug)Serial.println("C");
     //Serial.println(soil);
    
   }
@@ -110,17 +107,19 @@
   //  SoilSensor = map (SoilSensor, 212, 550, 100, 0); // -->DIY-- Mapeamos el valor del sensor de 0 a 100.
    
    
-    if (SoilSensor < 70) // Si el sensor detecta menos de 30%H
+    if (SoilSensor < 30) // Si el sensor detecta menos de 30%H
     {
       digitalWrite(pump, HIGH);
+     // digitalWrite(amarillo_seco_pin, HIGH);
       digitalWrite(verde_mojado_pin, HIGH);
       
     }
-    if (SoilSensor > (70 + histeresisH))// Si detecta mas de 90%H
+    if (SoilSensor > (30 + histeresisH))// Si detecta mas de 90%H
     {
       digitalWrite(pump, LOW);
+      digitalWrite(amarillo_seco_pin, LOW);
       digitalWrite(verde_mojado_pin, HIGH);
-      delay(10);
+      delay(100);
       digitalWrite(verde_mojado_pin, LOW);
       
     }
@@ -129,26 +128,18 @@
     
     Ldr = analogRead(A1);
     Ldr = map (Ldr, 0, 1023, 100, 0);             // Mapeamos el valor del sensor de 0 a 100.
-  if (Ldr < 45)
-  /*// if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    */
+  if (Ldr < 60)
     {
     digitalWrite(luz, HIGH);
-    digitalWrite(amarillo_luz_pin, HIGH);
-    delay(10);
-    digitalWrite(amarillo_luz_pin, LOW);
+    digitalWrite(amarillo_seco_pin, HIGH);
+    delay(100);
+    digitalWrite(amarillo_seco_pin, LOW);
   }
-  if (Ldr > (45 - histeresisL))
+  if (Ldr > (60 - histeresisL))
   
   {
     digitalWrite(luz, LOW);
-    //digitalWrite(rojo_temp_pin, LOW);
+    digitalWrite(amarillo_seco_pin, LOW);
   }
   
   
@@ -159,17 +150,11 @@
     
      if (TempC > 27)
     {
-   /*    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }*/
       myservo.write(70);     
       digitalWrite(fan, HIGH);
       digitalWrite(rojo_temp_pin, HIGH);
-      delay(10);
-      digitalWrite(rojo_temp_pin, LOW);
+      
+     
    }
   
     if (TempC < (27 - histeresisT))
@@ -182,3 +167,4 @@
   
   }
   
+
